a9d57a32d8d3143649db3a0f4aed79c53703025d
曹田亮 <footprint_c@163.com>
2022-11-11 23:17:54 +0800

    hotfix-1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git ~");
         System.out.println("第二次修改");
+        System.out.println("hellow hot fix");
     }
 }
