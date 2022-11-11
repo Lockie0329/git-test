00d8dd5ec6fa6f444cb77b68e21fe1aec1ab1678
曹田亮 <footprint_c@163.com>
2022-11-11 23:22:48 +0800

    masterConmmit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,7 @@ public class GitTest {
         System.out.println("hellow git ~");
         System.out.println("第二次修改");
         System.out.println("hellow hot fix");
+        //代码冲突
+        System.out.println("合并前做修改");
     }
 }
